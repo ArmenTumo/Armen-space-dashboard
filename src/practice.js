264b99de4c5099b212b727d20b91d92906df5a9b
Student <armen.galstyan3@tumo.org>
2026-05-19 19:50:13 +0400

    describe what you did

---
+++ src/practice.js
@@ -0,0 +1,21 @@
+function KmForMl(coord) {
+   return coord * 0.621;  
+}
+console.log(KmForMl(10));
+
+function Hi(name, ttt) {
+   name = "Hello, my name is Armen."
+   ttt = " I am 16 years old and a first-year college student."
+    return name+ttt;
+}
+console.log(Hi());
+
+
+const issPosition = {
+  latitude: 44,
+  longitude: 55,
+
+}
+console.log(issPosition["latitude"]);
+console.log(issPosition["longitude"]);
+

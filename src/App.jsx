7e1136b7ca84d50816f821f805fd0f460b2673fb
Student <armen.galstyan3@tumo.org>
2026-05-21 19:06:20 +0400

    vonc es

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,37 @@
-
+import './App.css'
 export default function kkk(){
   return (
     <div className="dashboard">
       <h1>Armen-space-dashboard</h1>
       <p>20.05.2026</p>
-      <Kamo />
-      <Karen />
+      <Kamo latitude="42.36" longitude="-71.05" />
+      <Karen name="Artemis 2" status="Active" />
+      <Tak name="ISS Expidition 71" status="Ongoing" />
     </div>
   )
 }
-function Kamo(){
+function Kamo(props){
  return (
-  <div className="kamo">
-   <h2>Karen cavt tanem</h2>
-   <p>inch ka ape</p>
+  <div className="card">
+   <h2>{props.latitude}</h2>
+   <p>{props.longitude}</p>
   </div>
  )
 }
-function Karen(){
+function Karen(props){
+  return (
+<div className="warning">
+<h3>{props.name}</h3>
+  <p>{props.status}</p>
+</div>
+
+  )
+}
+function Tak(props){
   return (
 <div className="card">
-  <h3>petqa xarnvenq ape</h3>
-  <p>che lava</p>
+<h3>{props.name}</h3>
+  <p>{props.status}</p>
 </div>
 
   )
